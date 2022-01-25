cd3786bd77ff7af258e6471cef19b1255629a593
jun117 <kbokro@icloud.com>
2022-01-25 10:02:09 +0900

    B-3　改行できない B-4　挫折した

--- B-3.py
+++ B-3.py
@@ -17,5 +17,9 @@ number2 = input('列数を入力してください：')
 
 for count1 in range(1, int(number1) + 1):
     for count2 in range(1, int(number2) + 1):
-        print(str(count2) + '×' + str(count1) + '=' + str(count2 * count1), end='')
+        result = count2 * count1
+
+        print(str(count2) + '×' + str(count1) + '=' + str(result).rjust(2) + '|', end=' ')
+
         print()
+# 　end=''で横並びがうまくできない
--- B-4.py
+++ B-4.py
@@ -1,7 +1,29 @@
-count2 = 3
-count1 = 4
+"""
+3都府県のいくつかの駅名とある日の最高気温のデータを辞書として持っています
+このデータを使って3つの問を満たす実装をしてください
+def main():
+    # 3都府県のいくつかの駅名とある日の最高気温(単位: ℃)のデータを辞書として持っています
+    weather_information = [
+        {'prefecture': '東京都', 'station': '渋谷', 'temperature': 6.5},
+        {'prefecture': '東京都', 'station': '池袋', 'temperature': 7.0},
+        {'prefecture': '東京都', 'station': '新橋', 'temperature': 7.5},
 
-result = count2 * count1
+        {'prefecture': '大阪府', 'station': '梅田', 'temperature': 8.2},
+        {'prefecture': '大阪府', 'station': '大阪', 'temperature': 9.3},
+        {'prefecture': '大阪府', 'station': '堺', 'temperature': 9.5},
 
-if result < 10:
-    print(str(result).rjust(10))
+        {'prefecture': '福岡県', 'station': '博多', 'temperature': 13.0},
+        {'prefecture': '福岡県', 'station': '太宰府', 'temperature': 15.0},
+    ]
+
+    # Q1. 全国の平均気温を計算してください(9.5となればOK)
+
+    # Q2. 大阪府のすべての駅名をカンマ区切りで出力してください( '梅田,大阪,堺' となればOK)
+
+    # Q3. 福岡県の平均気温を計算してください(14.0となればOK)
+
+
+if __name__ == '__main__':
+    main()
+"""
+# 辞書からtemperatureのvalueを複数取り出すのが分からないです。おしえてください。
