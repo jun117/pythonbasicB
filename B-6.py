370c2108cc73e8a86cdcd2890e0662dc4f4d96d3
jun117 <kbokro@icloud.com>
2022-01-26 22:11:11 +0900

    とりあえずB-6　完了

--- B-6.py
+++ B-6.py
@@ -1,39 +1,33 @@
-def goukei():
-    score_sum = 0
-    for score in scores:
-        score_sum = score_sum + score
-    return score_sum
+"""
+N面サイコロをM回振ったときの結果を表示してください
+N, M は正の整数とします
+N, M はユーザーからの入力を利用すること
+
+サイコロの面の数は?: 8
+何回振りますか?: 20
+[6, 6, 8, 5, 1, 6, 4, 4, 3, 4, 7, 5, 7, 1, 4, 2, 5, 7, 1, 7]
 
 
-# splitで文字列を区切り文字を分割しリスト化
-scores = list(map(int, input("入力").split()))
+# サイコロの1面の場合　1
+# サイコロ面2　　　1　2
+　サイコロの面3　　1　2　3
 
-print("合計値:" + str(goukei()))
-
-
-def saidai():
-    max_s = 0
-    for score in scores:
-        if max_s < score:
-            max_s = score
-    return max_s
+サイコロの面　4　1234
+サイコロの面　5　12345
+"""
 
 
-print(saidai())
+def square():
+    import random
+
+    r = random.choice(range(1, int(number1) + 1))
+    return (r)
 
 
-def saisyo():
-    min_s = scores[0]
-    for score in scores:
-        if score < min_s:
-            min_s = score
-    return min_s
+number1 = input('サイコロの面数は？：')
+number2 = input('何回振りますか？：')
 
+for num in range(int(number2)):
+    # この場合forの後ろの変数はどういう意味があるのでしょか？下のprintを繰り返させるだけすか？
 
-print(saisyo())
-
-score_sum = 0
-for score in scores:
-    score_sum = score_sum + score
-    result = score_sum / (len(score) - 1)
-print(result)
+    print(square(), end=',')
--- B-7.py
+++
@@ -1,9 +0,0 @@
-scores = list(map(int, input("入力").split()))
-
-score_sum = 0
-for score in scores:
-    score_sum = score_sum + score
-
-result = int(score_sum) / len(scores)
-
-print(result)
